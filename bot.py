cee9f5361dcd3864fc700cfad038aea757c9a785
Iskak Toltay <y.toltay@lancaster.ac.uk>
2022-06-07 20:10:30 +0600

    separated message sending to a function

--- bot.py
+++ bot.py
@@ -1,27 +1,22 @@
 import pyautogui as pag
 
 pag.FAILSAFE = True
-# startingPoint = pag.position()
-# print(startingPoint)
-lastPoint = NULL
 
-while(True):
+def sendMessage():
     isConfirmed = pag.confirm("Do you want to send the message?")
     if isConfirmed != "Cancel":
         pag.click()
         pag.hotkey('ctrl', 'v')
         pag.press('enter')
-    pag.moveRel(0, 107, 0)
+
+while(True):
+    sendMessage()
+    pag.moveRel(0, 107, 0.5)
     if pag.position().y >= 1300:
+        sendMessage()
         break
 
 while(True):
-    # pag.PAUSE = 1
+    pag.PAUSE = 0.5
     pag.scroll(-86)
-    isConfirmed = pag.confirm("Do you want to send the message?")
-    if isConfirmed != "Cancel":
-        pag.click()
-        pag.hotkey('ctrl', 'v')
-        pag.press('enter')
-# print(pag.position())
-# print(pag.size())
+        sendMessage()
